2bbda509862e6dd3bd3fac99c9ae12afd5006006
Erik Heemskerk <erikheemskerk+github@gmail.com>
2024-08-06 10:57:08 +0200

    Remove debug stuff from site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,43 +3,23 @@
     for (let i = 0; i < sortables.length; i++) {
         const sortable = sortables[i];
         const sortableInstance = new Sortable(sortable, {
-            animation: 150,
             ghostClass: 'blue-background-class',
-            group: 'tickets',
-            onChoose: function (evt) {
-                console.log("onChoose: ", sortable.attributes["data-lane"]);
-            },
-            onUnchoose: function (evt) {
-                console.log("onUnchoose: ", sortable.attributes["data-lane"]);
-            },
-            onStart: function (evt) {
-                console.log("onStart: ", sortable.attributes["data-lane"]);
-            },
-            onEnd: function (evt) {
-                console.log("onEnd: ", sortable.attributes["data-lane"]);
-            },
-            onAdd: function (evt) {
-                console.log("onAdd: ", sortable.attributes["data-lane"]);
-            },
-            onUpdate: function (evt) {
-                console.log("onUpdate: ", sortable.attributes["data-lane"]);
-            },
-            onRemove: function (evt) {
-                console.log("onRemove: ", sortable.attributes["data-lane"]);
-            },
-            onMove: function (evt) {
-                console.log("onMove: ", sortable.attributes["data-lane"]);
-            },
+            group: 'cards',
         });
         
-        // htmx.on("#lanes", "htmx:trigger", function (evt) {
-        //     //console.debug("Sortable: htmx:trigger: ", evt);
-        //     sortableInstance.option("disabled", true);
-        // });
+        // disable sortable whenever a request is triggered on a .sortable
+        htmx.on("htmx:trigger", function(evt) {
+            if (evt.target.className !== "sortable")
+                return;
 
-        // htmx.on("#lanes", "htmx:afterSwap", function(evt) {
-        //     //console.debug("Sortable: htmx:afterSwap: ", evt);
-        //     sortableInstance.option("disabled", false);
-        // });
+            sortableInstance.option("disabled", true);
+        });
+
+        htmx.on("htmx:afterRequest", function(evt) {
+            if (evt.target.className !== "sortable")
+                return;
+
+            sortableInstance.option("disabled", false);
+        });
     }
 })
