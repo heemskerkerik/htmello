1e8c5930eba5067d4d20e7669667a597d4118a50
Erik Heemskerk <heemskerkerik@users.noreply.github.com>
2025-01-04 11:14:12 +0100

    Rename 'id' parameter to boardId

--- Board/BoardController.cs
+++ Board/BoardController.cs
@@ -7,10 +7,10 @@ namespace htmx_trello.Board;
 [Controller]
 public class BoardController(BoardService boardService): Controller
 {
-    [HttpGet("/boards/{id:guid}")]
-    public IActionResult ViewBoard(Guid id)
+    [HttpGet("/boards/{boardId:guid}")]
+    public IActionResult ViewBoard(Guid boardId)
     {
-        var board = boardService.GetById(id);
+        var board = boardService.GetById(boardId);
 
         if (board is null)
         {
@@ -26,45 +26,45 @@ public class BoardController(BoardService boardService): Controller
     public IActionResult CreateBoard([FromForm(Name = "Board")] CreateBoardRequest request)
     {
         var newBoard = boardService.Add(request.Name, request.Color);
-        string newBoardUrl = Url.Action("ViewBoard", "Board", new { id = newBoard.Id })!;
+        string newBoardUrl = Url.Action("ViewBoard", "Board", new { boardId = newBoard.Id })!;
 
         Response.Htmx(h => h.Redirect(newBoardUrl));
         return Redirect(newBoardUrl);
     }
 
-    [HttpGet("/boards/{id:guid}/editName")]
-    public IActionResult ShowEditBoardName(Guid id)
+    [HttpGet("/boards/{boardId:guid}/editName")]
+    public IActionResult ShowEditBoardName(Guid boardId)
     {
-        var board = boardService.GetById(id);
+        var board = boardService.GetById(boardId);
 
         return View("_EditBoardName", board);
     }
     
-    [HttpGet("/boards/{id:guid}/name")]
-    public IActionResult ShowBoardName(Guid id)
+    [HttpGet("/boards/{boardId:guid}/name")]
+    public IActionResult ShowBoardName(Guid boardId)
     {
-        var board = boardService.GetById(id);
+        var board = boardService.GetById(boardId);
 
         return View("_BoardName", board);
     }
 
-    [HttpPut("/boards/{id:guid}/editName")]
+    [HttpPut("/boards/{boardId:guid}/editName")]
     [ValidateAntiForgeryToken]
     public IActionResult EditBoardName(
-        Guid id,
+        Guid boardId,
         [FromForm] EditBoardNameRequest request
     )
     {
-        boardService.SetBoardName(id, request.Name);
-        var editedBoard = boardService.GetById(id);
+        boardService.SetBoardName(boardId, request.Name);
+        var editedBoard = boardService.GetById(boardId);
 
         return View("_BoardName", editedBoard);
     }
 
-    [HttpGet("/boards/{id:guid}/addLaneForm")]
-    public IActionResult ShowAddLaneForm(Guid id)
+    [HttpGet("/boards/{boardId:guid}/addLaneForm")]
+    public IActionResult ShowAddLaneForm(Guid boardId)
     {
-        var board = boardService.GetById(id);
+        var board = boardService.GetById(boardId);
 
         return View("_AddLane", board);
     }
